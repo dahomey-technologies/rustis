3eae6e011b9044c94fb774a2eed6c6286481f217
Michaël Catanzariti <michael.catanzariti@gmail.com>
2022-09-16 02:56:53 +0200

    replaced numtoa by itoa

--- src/commands/command_encoder.rs
+++ src/commands/command_encoder.rs
@@ -1,6 +1,5 @@
 use crate::{resp::BulkString, Command, CommandArgs, Error, Result};
 use bytes::{BufMut, BytesMut};
-use numtoa::NumToA;
 use tokio_util::codec::Encoder;
 
 pub(crate) struct CommandEncoder;
@@ -27,13 +26,13 @@ fn encode_bulkstring(bulk_string: &BulkString, buf: &mut BytesMut) {
     match bulk_string {
         BulkString::Nil => buf.put(&b"$-1\r\n"[..]),
         BulkString::Integer(i) => {
-            let mut temp = [0u8; 21];
-            let temp = i.numtoa(10, &mut temp[..]);            
+            let mut temp = itoa::Buffer::new();
+            let str = temp.format(*i);        
 
             buf.put_u8(b'$');
-            encode_integer(temp.len() as i64, buf);
+            encode_integer(str.len() as i64, buf);
             encode_crlf(buf);
-            buf.put(temp);
+            buf.put(str.as_bytes());
             encode_crlf(buf);
         },
         _ => {
@@ -76,8 +75,9 @@ fn encode_command_args(command_args: &CommandArgs, buf: &mut BytesMut) {
 }
 
 fn encode_integer(i: i64, buf: &mut BytesMut) {
-    let mut buffer = [0u8; 21];
-    buf.put(i.numtoa(10, &mut buffer[..]));
+    let mut buffer = itoa::Buffer::new();
+    let str = buffer.format(i);
+    buf.put(str.as_bytes());
 }
 
 fn encode_crlf(buf: &mut BytesMut) {

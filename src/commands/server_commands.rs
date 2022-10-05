e2fcf15cd9ef2143dc53081c5b33cd42241dfacb
Michaël Catanzariti <michael.catanzariti@gmail.com>
2022-10-05 11:05:57 +0200

    added all ACL commands

--- src/commands/server_commands.rs
+++ src/commands/server_commands.rs
@@ -1,7 +1,7 @@
 use crate::{
     resp::{
-        cmd, BulkString, CommandArgs, FromValue, IntoArgs, KeyValueArgOrCollection,
-        SingleArgOrCollection, FromKeyValueValueArray,
+        cmd, BulkString, CommandArgs, FromKeyValueValueArray, FromSingleValueArray, FromValue,
+        IntoArgs, KeyValueArgOrCollection, SingleArgOrCollection, Value,
     },
     CommandResult, PrepareCommand,
 };
@@ -9,7 +9,202 @@ use crate::{
 /// A group of Redis commands related to Server Management
 /// # See Also
 /// [Redis Server Management Commands](https://redis.io/commands/?group=server)
+/// [ACL guide](https://redis.io/docs/manual/security/acl/)
 pub trait ServerCommands<T>: PrepareCommand<T> {
+    /// The command shows the available ACL categories if called without arguments.
+    /// If a category name is given, the command shows all the Redis commands in the specified category.
+    ///
+    /// # Return
+    /// A collection of ACL categories or a collection of commands inside a given category.
+    ///
+    /// # Errors
+    /// The command may return an error if an invalid category name is given as argument.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-cat/>](https://redis.io/commands/acl-cat/)
+    fn acl_cat<C, CC>(&self, options: AclCatOptions) -> CommandResult<T, CC>
+    where
+        C: FromValue,
+        CC: FromSingleValueArray<C>,
+    {
+        self.prepare_command(cmd("ACL").arg("CAT").arg(options))
+    }
+
+    /// Delete all the specified ACL users and terminate all 
+    /// the connections that are authenticated with such users.
+    ///
+    /// # Return
+    /// The number of users that were deleted. 
+    /// This number will not always match the number of arguments since certain users may not exist.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-deluser/>](https://redis.io/commands/acl-deluser/)
+    fn acl_deluser<U, UU>(&self, usernames: UU) -> CommandResult<T, usize>
+    where
+        U: Into<BulkString>,
+        UU: SingleArgOrCollection<U>,
+    {
+        self.prepare_command(cmd("ACL").arg("DELUSER").arg(usernames))
+    }
+
+    /// Simulate the execution of a given command by a given user. 
+    ///
+    /// # Return
+    /// OK on success.
+    /// An error describing why the user can't execute the command.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-dryrun/>](https://redis.io/commands/acl-dryrun/)
+    fn acl_dryrun<U, C, R>(&self, username: U, command: C, options: AclDryRunOptions) -> CommandResult<T, R>
+    where
+        U: Into<BulkString>,
+        C: Into<BulkString>,
+        R: FromValue
+    {
+        self.prepare_command(cmd("ACL").arg("DRYRUN").arg(username).arg(command).arg(options))
+    }
+
+    /// Generates a password starting from /dev/urandom if available, 
+    /// otherwise (in systems without /dev/urandom) it uses a weaker 
+    /// system that is likely still better than picking a weak password by hand.
+    ///
+    /// # Return
+    /// by default 64 bytes string representing 256 bits of pseudorandom data. 
+    /// Otherwise if an argument if needed, the output string length is the number 
+    /// of specified bits (rounded to the next multiple of 4) divided by 4.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-genpass/>](https://redis.io/commands/acl-genpass/)
+    fn acl_genpass<R: FromValue>(&self, options: AclGenPassOptions) -> CommandResult<T, R>
+    {
+        self.prepare_command(cmd("ACL").arg("GENPASS").arg(options))
+    }
+
+    /// The command returns all the rules defined for an existing ACL user.
+    ///
+    /// # Return
+    /// A collection of ACL rule definitions for the user.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-getuser/>](https://redis.io/commands/acl-getuser/)
+    fn acl_getuser<U, RR>(&self, username: U) -> CommandResult<T, RR>
+    where
+        U: Into<BulkString>,
+        RR: FromKeyValueValueArray<String, Value>
+
+    {
+        self.prepare_command(cmd("ACL").arg("GETUSER").arg(username))
+    }
+
+    /// The command shows the currently active ACL rules in the Redis server.
+    ///
+    /// # Return
+    /// An array of strings.
+    /// Each line in the returned array defines a different user, and the 
+    /// format is the same used in the redis.conf file or the external ACL file
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-list/>](https://redis.io/commands/acl-list/)
+    fn acl_list(&self) -> CommandResult<T, Vec<String>>
+    {
+        self.prepare_command(cmd("ACL").arg("LIST"))
+    }
+
+    /// When Redis is configured to use an ACL file (with the aclfile configuration option), 
+    /// this command will reload the ACLs from the file, replacing all the current ACL rules 
+    /// with the ones defined in the file. 
+    ///
+    /// # Return
+    /// An array of strings.
+    /// Each line in the returned array defines a different user, and the 
+    /// format is the same used in the redis.conf file or the external ACL file
+    /// 
+    /// # Errors
+    /// The command may fail with an error for several reasons: 
+    /// - if the file is not readable, 
+    /// - if there is an error inside the file, and in such case the error will be reported to the user in the error.
+    /// - Finally the command will fail if the server is not configured to use an external ACL file.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-load/>](https://redis.io/commands/acl-load/)
+    fn acl_load(&self) -> CommandResult<T, ()>
+    {
+        self.prepare_command(cmd("ACL").arg("LOAD"))
+    }
+
+    /// The command shows a list of recent ACL security events
+    ///
+    /// # Return
+    /// A key/value collection of ACL security events.
+    /// Empty collection when called with the [`reset`](crate::AclLogOptions::reset) option
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-log/>](https://redis.io/commands/acl-log/)
+    fn acl_log<EE>(&self, options: AclLogOptions) -> CommandResult<T, Vec<EE>>
+    where 
+        EE: FromKeyValueValueArray<String, Value>
+    {
+        self.prepare_command(cmd("ACL").arg("LOG").arg(options))
+    }
+
+    /// When Redis is configured to use an ACL file (with the aclfile configuration option), 
+    /// this command will save the currently defined ACLs from the server memory to the ACL file.
+    /// 
+    /// # Errors
+    /// The command may fail with an error for several reasons: 
+    /// - if the file cannot be written 
+    /// - if the server is not configured to use an external ACL file.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-save/>](https://redis.io/commands/acl-save/)
+    fn acl_save(&self) -> CommandResult<T, ()>
+    {
+        self.prepare_command(cmd("ACL").arg("SAVE"))
+    }
+
+    /// Create an ACL user with the specified rules or modify the rules of an existing user. 
+    /// 
+    /// # Errors
+    /// If the rules contain errors, the error is returned.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-setuser/>](https://redis.io/commands/acl-setuser/)
+    fn acl_setuser<U, R, RR>(&self, username: U, rules: RR) -> CommandResult<T, ()>
+    where
+        U: Into<BulkString>,
+        R: Into<BulkString>,
+        RR: SingleArgOrCollection<R>
+    {
+        self.prepare_command(cmd("ACL").arg("SETUSER").arg(username).arg(rules))
+    }
+
+    /// The command shows a list of all the usernames of the currently configured users in the Redis ACL system.
+    /// 
+    /// # Return
+    /// A collection of usernames
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-users/>](https://redis.io/commands/acl-users/)
+    fn acl_users<U, UU>(&self) -> CommandResult<T, UU>
+    where
+        U: FromValue,
+        UU: FromSingleValueArray<U>,
+    {
+        self.prepare_command(cmd("ACL").arg("USERS"))
+    }
+
+    /// Return the username the current connection is authenticated with. 
+    /// 
+    /// # Return
+    /// The username of the current connection.
+    ///
+    /// # See Also
+    /// [<https://redis.io/commands/acl-whoami/>](https://redis.io/commands/acl-whoami/)
+    fn acl_whoami<U: FromValue>(&self) -> CommandResult<T, U>
+    {
+        self.prepare_command(cmd("ACL").arg("WHOAMI"))
+    }
+    
     /// Used to read the configuration parameters of a running Redis server.
     ///
     /// For every key that does not hold a string value or does not exist,
@@ -19,14 +214,14 @@ pub trait ServerCommands<T>: PrepareCommand<T> {
     /// Array reply: collection of the requested params with their matching values.
     ///
     /// # See Also
-    /// [https://redis.io/commands/mget/](https://redis.io/commands/mget/)
+    /// [<https://redis.io/commands/mget/>](https://redis.io/commands/mget/)
     #[must_use]
     fn config_get<P, PP, V, VV>(&self, params: PP) -> CommandResult<T, VV>
     where
         P: Into<BulkString>,
         PP: SingleArgOrCollection<P>,
         V: FromValue,
-        VV: FromKeyValueValueArray<String, V>
+        VV: FromKeyValueValueArray<String, V>,
     {
         self.prepare_command(cmd("CONFIG").arg("GET").arg(params))
     }
@@ -34,7 +229,7 @@ pub trait ServerCommands<T>: PrepareCommand<T> {
     /// Used in order to reconfigure the server at run time without the need to restart Redis.
     ///
     /// # See Also
-    /// [https://redis.io/commands/config-set/](https://redis.io/commands/config-set/)
+    /// [<https://redis.io/commands/config-set/>](https://redis.io/commands/config-set/)
     #[must_use]
     fn config_set<P, V, C>(&self, configs: C) -> CommandResult<T, ()>
     where
@@ -48,7 +243,7 @@ pub trait ServerCommands<T>: PrepareCommand<T> {
     /// Delete all the keys of the currently selected DB.
     ///
     /// # See Also
-    /// [https://redis.io/commands/flushdb/](https://redis.io/commands/flushdb/)
+    /// [<https://redis.io/commands/flushdb/>](https://redis.io/commands/flushdb/)
     #[must_use]
     fn flushdb(&self, flushing_mode: FlushingMode) -> CommandResult<T, ()> {
         self.prepare_command(cmd("FLUSHDB").arg(flushing_mode))
@@ -57,7 +252,7 @@ pub trait ServerCommands<T>: PrepareCommand<T> {
     /// Delete all the keys of all the existing databases, not just the currently selected one.
     ///
     /// # See Also
-    /// [https://redis.io/commands/flushall/](https://redis.io/commands/flushall/)
+    /// [<https://redis.io/commands/flushall/>](https://redis.io/commands/flushall/)
     #[must_use]
     fn flushall(&self, flushing_mode: FlushingMode) -> CommandResult<T, ()> {
         self.prepare_command(cmd("FLUSHALL").arg(flushing_mode))
@@ -67,7 +262,7 @@ pub trait ServerCommands<T>: PrepareCommand<T> {
     /// a Unix timestamp and the amount of microseconds already elapsed in the current second.
     ///
     /// # See Also
-    /// [https://redis.io/commands/time/](https://redis.io/commands/time/)
+    /// [<https://redis.io/commands/time/>](https://redis.io/commands/time/)
     #[must_use]
     fn time(&self) -> CommandResult<T, (u32, u32)> {
         self.prepare_command(cmd("TIME"))
@@ -96,5 +291,111 @@ impl IntoArgs for FlushingMode {
             FlushingMode::Async => args.arg("ASYNC"),
             FlushingMode::Sync => args.arg("SYNC"),
         }
+    }
+}
+
+/// Options for the [`acl_cat`](crate::ServerCommands::acl_cat) command
+#[derive(Default)]
+pub struct AclCatOptions {
+    command_args: CommandArgs,
+}
+
+impl AclCatOptions {
+    #[must_use]
+    pub fn category_name<C: Into<BulkString>>(self, category_name: C) -> Self {
+        Self {
+            command_args: self.command_args.arg(category_name),
+        }
+    }
+}
+
+impl IntoArgs for AclCatOptions {
+    fn into_args(self, args: CommandArgs) -> CommandArgs {
+        args.arg(self.command_args)
+    }
+}
+
+/// Options for the [`acl_dryrun`](crate::ServerCommands::acl_dryrun) command
+#[derive(Default)]
+pub struct AclDryRunOptions {
+    command_args: CommandArgs,
+}
+
+impl AclDryRunOptions {
+    #[must_use]
+    pub fn arg<A, AA>(self, args: AA) -> Self 
+    where
+        A: Into<BulkString>,
+        AA: SingleArgOrCollection<A>
+    {
+        Self {
+            command_args: self.command_args.arg(args),
+        }
+    }
+}
+
+impl IntoArgs for AclDryRunOptions {
+    fn into_args(self, args: CommandArgs) -> CommandArgs {
+        args.arg(self.command_args)
+    }
+}
+
+/// Options for the [`acl_genpass`](crate::ServerCommands::acl_genpass) command
+#[derive(Default)]
+pub struct AclGenPassOptions {
+    command_args: CommandArgs,
+}
+
+impl AclGenPassOptions {
+    /// The command output is a hexadecimal representation of a binary string. 
+    /// By default it emits 256 bits (so 64 hex characters). 
+    /// The user can provide an argument in form of number of bits to emit from 1 to 1024 to change the output length. 
+    /// Note that the number of bits provided is always rounded to the next multiple of 4. 
+    /// So for instance asking for just 1 bit password will result in 4 bits to be emitted, in the form of a single hex character.
+    #[must_use]
+    pub fn bits(self, bits: usize) -> Self 
+    {
+        Self {
+            command_args: self.command_args.arg(bits),
+        }
+    }
+}
+
+impl IntoArgs for AclGenPassOptions {
+    fn into_args(self, args: CommandArgs) -> CommandArgs {
+        args.arg(self.command_args)
+    }
+}
+
+/// Options for the [`acl_log`](crate::ServerCommands::acl_log) command
+#[derive(Default)]
+pub struct AclLogOptions {
+    command_args: CommandArgs,
+}
+
+impl AclLogOptions {
+    /// This optional argument specifies how many entries to show. 
+    /// By default up to ten failures are returned.
+    #[must_use]
+    pub fn count(self, count: usize) -> Self 
+    {
+        Self {
+            command_args: self.command_args.arg(count),
+        }
+    }
+
+    /// The special RESET argument clears the log. 
+    #[must_use]
+    pub fn reset(self) -> Self 
+    {
+        Self {
+            command_args: self.command_args.arg("RESET"),
+        }
+    }
+}
+
+impl IntoArgs for AclLogOptions {
+    fn into_args(self, args: CommandArgs) -> CommandArgs {
+        args.arg(self.command_args)
     }
 }
--- src/tests/server_commands.rs
+++ src/tests/server_commands.rs
@@ -1,22 +1,259 @@
-use std::collections::HashMap;
-
 use crate::{
     resp::{BulkString, Value},
     tests::get_test_client,
-    ConnectionCommands, FlushingMode, Result, ServerCommands, StringCommands,
+    AclCatOptions, AclDryRunOptions, AclGenPassOptions, AclLogOptions, ClientInfo,
+    ConnectionCommands, Error, FlushingMode, Result, ServerCommands, StringCommands,
 };
 use serial_test::serial;
+use std::collections::{HashMap, HashSet};
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_cat() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    let categories: Vec<String> = client.acl_cat(AclCatOptions::default()).await?;
+    assert!(categories.contains(&"dangerous".to_owned()));
+
+    let dangerous_commands: HashSet<String> = client
+        .acl_cat(AclCatOptions::default().category_name("dangerous"))
+        .await?;
+    assert!(dangerous_commands.contains(&"flushdb".to_owned()));
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_deluser() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    client.acl_setuser("foo", Vec::<String>::new()).await?;
+    client.acl_setuser("bar", Vec::<String>::new()).await?;
+    let deleted = client.acl_deluser(["foo", "bar"]).await?;
+    assert_eq!(2, deleted);
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_dryrun() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    client.acl_setuser("VIRGINIA", ["+SET", "~*"]).await?;
+    client
+        .acl_dryrun(
+            "VIRGINIA",
+            "SET",
+            AclDryRunOptions::default().arg("foo").arg("bar"),
+        )
+        .await?;
+    let result: String = client
+        .acl_dryrun("VIRGINIA", "GET", AclDryRunOptions::default().arg("foo"))
+        .await?;
+    assert_eq!(
+        "This user has no permissions to run the 'get' command",
+        result
+    );
+
+    client.acl_deluser("VIRGINIA").await?;
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_genpass() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    let password: String = client.acl_genpass(AclGenPassOptions::default()).await?;
+    assert_eq!(64, password.len());
+
+    let password: String = client
+        .acl_genpass(AclGenPassOptions::default().bits(32))
+        .await?;
+    assert_eq!(8, password.len());
+
+    let password: String = client
+        .acl_genpass(AclGenPassOptions::default().bits(5))
+        .await?;
+    assert_eq!(2, password.len());
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_getuser() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    client.acl_setuser("foo", Vec::<String>::new()).await?;
+    let rules: HashMap<String, Value> = client.acl_getuser("foo").await?;
+    // default `commands` rule
+    assert!(
+        matches!(rules.get("commands"), Some(Value::BulkString(BulkString::Binary(rule))) if rule == b"-@all")
+    );
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_list() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    let users: Vec<String> = client.acl_list().await?;
+    assert_eq!(1, users.len());
+    assert!(users[0].starts_with("user default"));
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_load() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    let result = client.acl_load().await;
+    assert!(
+        matches!(result, Err(Error::Redis(e)) if e.starts_with("ERR This Redis instance is not configured to use an ACL file."))
+    );
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_log() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    let result = client.auth(Some("someuser"), "wrongpassword").await;
+    assert!(result.is_err());
+
+    let logs: Vec<HashMap<String, Value>> =
+        client.acl_log(AclLogOptions::default().count(1)).await?;
+    assert_eq!(1, logs.len());
+    assert!(
+        matches!(logs[0].get("reason"), Some(Value::BulkString(BulkString::Binary(reason))) if reason == b"auth")
+    );
+    let client_info: String = logs[0].get("client-info").unwrap().to_string();
+    let client_info = ClientInfo::from_line(&client_info)?;
+    assert_eq!("auth", client_info.cmd);
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_save() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    let result = client.acl_save().await;
+    assert!(
+        matches!(result, Err(Error::Redis(e)) if e.starts_with("ERR This Redis instance is not configured to use an ACL file."))
+    );
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_setuser() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    // activate user + password + remove all key patterns + allow all commands
+    client
+        .acl_setuser("foo", ["on", ">pwd", "resetkeys", "allcommands"])
+        .await?;
+
+    client.auth(Some("foo"), "pwd").await?;
+
+    let result = client.set("key", "value").await;
+    assert!(matches!(result, Err(Error::Redis(e)) if e.starts_with("NOPERM")));
+
+    client.acl_setuser("foo", ["~key"]).await?;
+    let _rules: HashMap<String, Value> = client.acl_getuser("foo").await?;
+    client.set("key", "value").await?;
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_users() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    client.acl_setuser("foo", Vec::<String>::new()).await?;
+    client.acl_setuser("bar", Vec::<String>::new()).await?;
+
+    let users: Vec<String> = client.acl_users().await?;
+    assert_eq!(3, users.len());
+    assert_eq!("bar", users[0]);
+    assert_eq!("default", users[1]);
+    assert_eq!("foo", users[2]);
+
+    Ok(())
+}
+
+#[cfg_attr(feature = "tokio-runtime", tokio::test)]
+#[cfg_attr(feature = "async-std-runtime", async_std::test)]
+#[serial]
+async fn acl_whoami() -> Result<()> {
+    let client = get_test_client().await?;
+    client.flushall(FlushingMode::Sync).await?;
+
+    let current_user: String = client.acl_whoami().await?;
+    assert_eq!("default", current_user);
+
+    client.acl_setuser("foo", ["on", ">pwd", "+ACL|WHOAMI"]).await?;
+    client.auth(Some("foo"), "pwd").await?;
+    let current_user: String = client.acl_whoami().await?;
+    assert_eq!("foo", current_user);
+
+    Ok(())
+}
 
 #[cfg_attr(feature = "tokio-runtime", tokio::test)]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 #[serial]
 async fn config_get() -> Result<()> {
-    let client  = get_test_client().await?;
+    let client = get_test_client().await?;
 
-    let configs: HashMap<String, String> = client.config_get(["hash-max-listpack-entries", "zset-max-listpack-entries"]).await?;
+    let configs: HashMap<String, String> = client
+        .config_get(["hash-max-listpack-entries", "zset-max-listpack-entries"])
+        .await?;
     assert_eq!(2, configs.len());
-    assert_eq!(Some(&"512".to_owned()), configs.get("hash-max-listpack-entries"));
-    assert_eq!(Some(&"128".to_owned()), configs.get("zset-max-listpack-entries"));
+    assert_eq!(
+        Some(&"512".to_owned()),
+        configs.get("hash-max-listpack-entries")
+    );
+    assert_eq!(
+        Some(&"128".to_owned()),
+        configs.get("zset-max-listpack-entries")
+    );
 
     Ok(())
 }
@@ -25,16 +262,34 @@ async fn config_get() -> Result<()> {
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
 #[serial]
 async fn config_set() -> Result<()> {
-    let client  = get_test_client().await?;
+    let client = get_test_client().await?;
 
-    client.config_set([("hash-max-listpack-entries", 513), ("zset-max-listpack-entries", 129)]).await?;
+    client
+        .config_set([
+            ("hash-max-listpack-entries", 513),
+            ("zset-max-listpack-entries", 129),
+        ])
+        .await?;
 
-    let configs: HashMap<String, String> = client.config_get(["hash-max-listpack-entries", "zset-max-listpack-entries"]).await?;
+    let configs: HashMap<String, String> = client
+        .config_get(["hash-max-listpack-entries", "zset-max-listpack-entries"])
+        .await?;
     assert_eq!(2, configs.len());
-    assert_eq!(Some(&"513".to_owned()), configs.get("hash-max-listpack-entries"));
-    assert_eq!(Some(&"129".to_owned()), configs.get("zset-max-listpack-entries"));
+    assert_eq!(
+        Some(&"513".to_owned()),
+        configs.get("hash-max-listpack-entries")
+    );
+    assert_eq!(
+        Some(&"129".to_owned()),
+        configs.get("zset-max-listpack-entries")
+    );
 
-    client.config_set([("hash-max-listpack-entries", 512), ("zset-max-listpack-entries", 128)]).await?;
+    client
+        .config_set([
+            ("hash-max-listpack-entries", 512),
+            ("zset-max-listpack-entries", 128),
+        ])
+        .await?;
 
     Ok(())
 }

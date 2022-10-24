1f2df797f46fa914ca979280bca22d1a24e532cc
Michaël Catanzariti <michael.catanzariti@gmail.com>
2022-10-25 00:48:20 +0200

    hide HashMapExt

--- src/resp/from_value.rs
+++ src/resp/from_value.rs
@@ -459,7 +459,7 @@ where
 {
 }
 
-pub trait HashMapExt<K, V, S> {
+pub(crate) trait HashMapExt<K, V, S> {
     fn remove_with_result<Q: ?Sized>(&mut self, k: &Q) -> Result<V>
     where
         K: Borrow<Q> + Hash + Eq,

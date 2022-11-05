b790db0d8e1cd136f550ba7da50e6147854588e9
Michaël Catanzariti <michael.catanzariti@gmail.com>
2022-11-05 13:28:36 +0100

    fix docker for Redis Cluster

--- redis/cluster-entrypoint.sh
+++ redis/cluster-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sed -i "s/\$PORT/$PORT/g" /redis/cluster.conf
+sed -i "s/\$BUS_PORT/$BUS_PORT/g" /redis/cluster.conf
+sed -i "s/\$HOST_IP/$HOST_IP/g" /redis/cluster.conf
 redis-server /redis/cluster.conf

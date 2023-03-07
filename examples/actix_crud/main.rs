f81d3debc799ff8ab66f08264041318aa61ec304
Michaël Catanzariti <michael.catanzariti@gmail.com>
2023-03-07 20:30:30 +0100

    added actix_crud example

---
+++ examples/actix_crud/main.rs
@@ -0,0 +1,106 @@
+use actix_web::{delete, get, http::StatusCode, post, web, App, HttpServer};
+use rustis::{
+    client::Client,
+    commands::{GenericCommands, StringCommands},
+};
+use std::{
+    cell::RefCell,
+    fmt::{self, Display},
+    net::SocketAddr,
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // build rustis client
+    let redis = Client::connect("redis://127.0.0.1:6379").await.unwrap();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(RefCell::new(redis.clone())))
+            .service(read)
+            .service(update)
+            .service(delete)
+    })
+    .bind(&addr)?
+    .run()
+    .await
+}
+
+#[get("/{key}")]
+async fn read(
+    redis: web::Data<RefCell<Client>>,
+    key: web::Path<String>,
+) -> Result<String, ServiceError> {
+    let mut redis = redis.borrow_mut();
+    let value: Option<String> = redis.get(key.clone()).await?;
+    value.ok_or_else(|| {
+        ServiceError::new(
+            StatusCode::NOT_FOUND,
+            format!("Key `{key}` does not exist."),
+        )
+    })
+}
+
+#[post("/{key}")]
+async fn update(
+    redis: web::Data<RefCell<Client>>,
+    key: web::Path<String>,
+    value: Option<String>,
+) -> Result<&'static [u8], ServiceError> {
+    if value.is_none() {
+        return Err(ServiceError::new(
+            StatusCode::BAD_REQUEST,
+            "Value not provided",
+        ));
+    }
+    let mut redis = redis.borrow_mut();
+    redis.set(key.into_inner(), value).await?;
+    Ok(&[])
+}
+
+#[delete("/{key}")]
+async fn delete(
+    redis: web::Data<RefCell<Client>>,
+    key: web::Path<String>,
+) -> Result<&'static [u8], ServiceError> {
+    let mut redis = redis.borrow_mut();
+    let deleted = redis.del(key.clone()).await?;
+    if deleted > 0 {
+        Ok(&[])
+    } else {
+        Err(ServiceError::new(
+            StatusCode::NOT_FOUND,
+            format!("Key `{key}` does not exist."),
+        ))
+    }
+}
+
+#[derive(Debug)]
+struct ServiceError(StatusCode, String);
+
+impl ServiceError {
+    fn new(status_code: StatusCode, description: impl ToString) -> Self {
+        Self(status_code, description.to_string())
+    }
+}
+
+impl Display for ServiceError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&self.1)
+    }
+}
+
+impl actix_web::error::ResponseError for ServiceError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        self.0
+    }
+}
+
+impl From<rustis::Error> for ServiceError {
+    fn from(e: rustis::Error) -> Self {
+        eprintln!("rustis error: {e}");
+        ServiceError::new(StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
+    }
+}

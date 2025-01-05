c6f1f7bd62a084d11cc2493a9bcef55b70dc5386
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2025-01-06 00:27:23 +0400

    try remove serialize test, because redis responsing static result

--- src/tests/server_commands.rs
+++ src/tests/server_commands.rs
@@ -287,7 +287,6 @@ async fn acl_whoami() -> Result<()> {
 
 #[cfg_attr(feature = "tokio-runtime", tokio::test)]
 #[cfg_attr(feature = "async-std-runtime", async_std::test)]
-#[serial]
 async fn bgrewriteaof() -> Result<()> {
     let client = get_test_client().await?;
 

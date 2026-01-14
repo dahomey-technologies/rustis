a740ca6c62cdf71339e98d8cab67bfc2572bcaab
Michaël Catanzariti <michael.catanzariti@gmail.com>
2026-01-14 22:08:17 +0100

    Simplified docker compose for tests (#121)

--- redis/set_host_ip.cmd
+++ redis/set_host_ip.cmd
@@ -1,3 +1,3 @@
 @echo off
-for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr IPv4') do (SET HOST_IP=%%a)
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /C:"IPv4" ^| findstr /V "172."') do (SET HOST_IP=%%a)
 set HOST_IP=%HOST_IP: =%

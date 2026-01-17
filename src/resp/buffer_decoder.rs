5ca3b0603ca0773af3489d4d23f4a54f1b0a6bc2
Michaël Catanzariti <michael.catanzariti@gmail.com>
2026-01-18 00:25:52 +0100

    Optimized BufferDecoder

--- src/resp/buffer_decoder.rs
+++ src/resp/buffer_decoder.rs
@@ -16,14 +16,57 @@ impl Decoder for BufferDecoder {
         }
 
         let bytes = src.as_ref();
-        let mut deserializer = RespDeserializer::new(bytes);
-        let result = IgnoredAny::deserialize(&mut deserializer);
-        match result {
-            Ok(_) => Ok(Some(RespBuf::new(
-                src.split_to(deserializer.get_pos()).freeze(),
-            ))),
-            Err(Error::EOF) => Ok(None),
-            Err(e) => Err(e),
+        let tag = bytes[0];
+
+        match tag {
+            // CATEGORY 1 : Simple scan of the next CRLF
+            b'+' | b'-' | b':' | b'_' | b',' | b'#' => {
+                if let Some(pos) = memchr::memchr(b'\n', bytes) {
+                    return Ok(Some(RespBuf::new(src.split_to(pos + 1).freeze())));
+                }
+            }
+            // CATEGORY 2 : Size reading + jump
+            b'$' | b'!' | b'=' => {
+                if let Some(pos) = memchr::memchr(b'\n', bytes) {
+                    let line = &bytes[1..pos];
+                    if let Ok(len) = parse_integer(line) {
+                        if len == -1 {
+                            // Null bulk strings
+                            return Ok(Some(RespBuf::new(src.split_to(pos + 1).freeze())));
+                        }
+                        let total_size = pos + 1 + (len as usize) + 2;
+                        if bytes.len() >= total_size {
+                            return Ok(Some(RespBuf::new(src.split_to(total_size).freeze())));
+                        }
+                    }
+                }
+            }
+            // CATEGORY 3 : Containers -> Fallback To Serde
+            b'*' | b'%' | b'~' | b'>' => {
+                let mut deserializer = RespDeserializer::new(bytes);
+                let result = IgnoredAny::deserialize(&mut deserializer);
+                match result {
+                    Ok(_) => {
+                        return Ok(Some(RespBuf::new(
+                            src.split_to(deserializer.get_pos()).freeze(),
+                        )));
+                    }
+                    Err(Error::EOF) => return Ok(None),
+                    Err(e) => return Err(e),
+                }
+            }
+            _ => return Err(Error::Client("Invalid TAG".to_string())),
         }
+
+        Ok(None)
     }
+}
+
+fn parse_integer(input: &[u8]) -> Result<isize> {
+    atoi::atoi(input).ok_or_else(|| {
+        Error::Client(format!(
+            "Cannot parse integer from {}",
+            String::from_utf8_lossy(input)
+        ))
+    })
 }
